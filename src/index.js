02a7f675a41522331297c216acfa370a02c40618
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-05-13 17:34:05 -0700

    Imported: randomAnswer. Created: eightballAnswers array. Implemented: randsomAnswer with eightballAnswers

--- src/index.js
+++ src/index.js
@@ -6,10 +6,31 @@ import {
   inputController,
   submitController,
 } from './formControllers';
-import { checkErrors } from './utils';
+import { checkErrors, randomAnswer } from './utils';
 
 // DOM ELEMENTS
 const eightballForm = document.getElementById('eightballForm');
+const eightballAnswers = [
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes, definitely',
+  'You may rely on it',
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
+  'Reply hazy try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
+  "Don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Very doubtful',
+];
 
 // DELEGATION
 // -- eightball form
@@ -25,6 +46,12 @@ if (eightballForm) {
 
     submitController(errorStack, document.getElementById('ask'));
 
-    if (checkErrors(errorStack)) formReset(document.getElementById('ask'));
+    if (checkErrors(errorStack)) {
+      const selectedAnswer = randomAnswer(...eightballAnswers);
+
+      console.log(selectedAnswer);
+
+      formReset(document.getElementById('ask'));
+    }
   });
 }
